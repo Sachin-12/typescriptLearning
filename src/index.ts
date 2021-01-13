82934e512806cb419d65517c2491c735586fffee
sachin <sachindraragul@gmail.com>
2021-01-14 01:30:02 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+function add(int1: number, int2: number) {
+  return int1 + int2;
+}
+
+const person: {
+  name: string;
+  nickname?: string;
+  age: number;
+} = {
+  name: "sachin",
+  age: 25
+};
+
+let res = add(2, 32);
+console.log(res);
+console.log(person.name);
+console.log(person.nickname);
+console.log(person.age);
